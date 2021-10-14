3dc45dd4a59b95e35d932e4f47e380be5943c091
Romaquis <romaquis.ribeiro@jnmoura.com.br>
2021-10-14 20:56:44 -0300

    Incluído campo nome e endereço

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/cart.service';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
@@ -10,11 +11,23 @@ export class CartComponent implements OnInit {
 
   items = this.cartService.getItems();
 
+  checkoutForm = this.formBuilder.group({
+    name:'',
+    address:''
+  });
+
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
 
 }
