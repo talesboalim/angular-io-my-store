e15133827aa7e3a17e691f6df4adf7bb9ee4e520
Romaquis <romaquis.ribeiro@jnmoura.com.br>
2021-10-14 00:05:42 -0300

    adicionado produto no carrinho e visualização do carrinho

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,14 +8,22 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { HttpClientModule } from '@angular/common/http'
+import { ShippingComponent } from './shipping/shipping.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   imports: [
+    CommonModule,
     BrowserModule,
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent }
+      { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent}
     ])
   ],
   declarations: [
@@ -23,7 +31,9 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    ShippingComponent,
   ],
   bootstrap: [
     AppComponent
--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 import { Product, products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -11,12 +12,20 @@ import { Product, products } from '../products';
 export class ProductDetailsComponent implements OnInit {
   product: Product | undefined;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private cartService: CartService
+    ) { }
 
   ngOnInit(): void {
     const routParams = this.route.snapshot.paramMap;
     const productIdFromRoute = Number(routParams.get('productId'));
     this.product = products.find(product => product.id === productIdFromRoute);
+  }
+
+  addToCart(product: Product){
+    this.cartService.addToCart(product);
+    window.alert('Your Product has been added to the cart!')
   }
 
 }
